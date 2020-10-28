6813fcf466cfde780fa7259a5d45d4d8b8c5bff9
George Okumu <gokumu12@gmail.com>
2020-10-28 07:29:12 +0300

    A user is able to switch to register activity if not have account

--- app/src/main/java/com/moringa/geofood/LoginActivity.java
+++ app/src/main/java/com/moringa/geofood/LoginActivity.java
@@ -2,13 +2,34 @@ package com.moringa.geofood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mRegisterTextView){
+            Intent register = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(register);
+            finish();
+        }
+
     }
 }
