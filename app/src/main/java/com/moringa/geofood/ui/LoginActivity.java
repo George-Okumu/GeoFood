5846a582c7e602e846e2157a9585a25e9a118a47
George Okumu <gokumu12@gmail.com>
2020-10-28 11:24:43 +0300

    Setted el error massages upon empty Credentials

--- app/src/main/java/com/moringa/geofood/ui/LoginActivity.java
+++ app/src/main/java/com/moringa/geofood/ui/LoginActivity.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringa.geofood.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String TAG = " ";
+
+    @BindView(R.id.passwordLoginButton) Button mPasswordLoginButton;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
+
+    //Variable of firebaseAuth
+    private FirebaseAuth mLoginAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +33,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         ButterKnife.bind(this);
 
+        //Instance of firebase auth
+        mLoginAuth = FirebaseAuth.getInstance();
+
         mRegisterTextView.setOnClickListener(this);
+        mPasswordLoginButton.setOnClickListener(this);
     }
 
     @Override
@@ -33,5 +48,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             finish();
         }
 
+        if(v == mPasswordLoginButton){
+            loginWithPassword();
+        }
+
     }
+
+    public void loginWithPassword(){
+        String userEmail = mEmailEditText.getText().toString().trim();
+        String userPassword = mPasswordEditText.getText().toString().trim();
+
+        //if user email is empty
+        if(userEmail.equals(" ")){
+            mEmailEditText.setError("Please Enter your email");
+            return;
+        }
+
+        if(userPassword.equals(" ")){
+            mPasswordEditText.setError("Password Cannot be blank");
+        }
+    }
+
 }
