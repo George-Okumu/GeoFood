ad34e89ea763bd0e2d7e4228ad323e8480f0147a
George Okumu <gokumu12@gmail.com>
2020-10-28 11:54:17 +0300

    Added Progress Dialog

--- app/src/main/java/com/moringa/geofood/ui/LoginActivity.java
+++ app/src/main/java/com/moringa/geofood/ui/LoginActivity.java
@@ -3,6 +3,7 @@ package com.moringa.geofood.ui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringa.geofood.R;
 
 import java.util.concurrent.BlockingDeque;
@@ -33,6 +35,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     //Variable of firebaseAuth
     private FirebaseAuth mLoginAuth;
+    private  FirebaseAuth.AuthStateListener mAuthListener;
+
+    //progressDialog
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +47,37 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         ButterKnife.bind(this);
 
+        //Auth Progress Dialog method
+
+        createAuthProgressDialog();
+
         //Instance of firebase auth
         mLoginAuth = FirebaseAuth.getInstance();
 
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Intent takeUserToMainActivityIfCredentialsAreTrue = new Intent(LoginActivity.this, MainActivity.class);
+                    takeUserToMainActivityIfCredentialsAreTrue.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(takeUserToMainActivityIfCredentialsAreTrue);
+                    finish();
+                }
+            }
+        };
+
+
+
         mRegisterTextView.setOnClickListener(this);
         mPasswordLoginButton.setOnClickListener(this);
+    }
+    //Defining the method
+    private void createAuthProgressDialog(){
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("Connecting.......");
+        mProgressDialog.setMessage("Authorising with database........");
+        mProgressDialog.setCancelable(false);
     }
 
     @Override
@@ -76,17 +108,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             mPasswordEditText.setError("Password Cannot be blank");
         }
 
+        mProgressDialog.show();
         mLoginAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                mProgressDialog.dismiss();
                 Log.d(TAG, "Sign With Email :onComplete:" + task.isSuccessful());
 
                 if(!task.isSuccessful()){
                     Log.e(TAG, "onComplete: Sign InWith Email Failed", task.getException());
-                    Toast.makeText(LoginActivity.this, "Error Occured:Try giving correct details", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Error Occurred:Try giving correct details", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mLoginAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mLoginAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
